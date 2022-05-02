9abcad5e1c19b6400a15ef1eb6f19e1e4ec2509b
MaKnok <marinaknok@gmail.com>
2022-05-01 22:21:24 -0300

    listing posts

---
+++ .slicemachine/assets/slices/Posts/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Posts';
+
+export default {
+  title: 'slices/Posts'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"posts","items":[],"primary":{"title":[{"type":"heading1","text":"Seize intuitive blockchains","spans":[]}],"description":[{"type":"paragraph","text":"Deserunt ex laboris ad dolore incididunt culpa enim magna exercitation quis est officia.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,16 +20,43 @@ export default NextAuth({
     // ...add more providers here
   ],
   callbacks: {
-    async signIn( {user, account, profile, email, credentials }) {
+    async signIn({user, account, profile}) {
       
-      await fauna.query(
-        q.Create(
-          q.Collection('users'),
-          { data: { email } }
-        )
-      )
+      const { email } = user;
 
-      return true
+      try {
+
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+
+      } catch {
+
+        return false
+
+      }
+
     }, 
   } 
 })
